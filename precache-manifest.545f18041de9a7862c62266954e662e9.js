9fb138daac42d784ef3322961ff2d3f21c694a3b
Finn <finnbutler1@gmail.com>
2020-09-19 13:39:11 +0100

    Updates

---
+++ precache-manifest.545f18041de9a7862c62266954e662e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03752049115a8b9da0ef88b90bedbeb6",
+    "url": "/drinkinggames/index.html"
+  },
+  {
+    "revision": "137f80fb6403ef908a98",
+    "url": "/drinkinggames/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b851b15b666fef468c2",
+    "url": "/drinkinggames/static/js/2.a529106b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drinkinggames/static/js/2.a529106b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "137f80fb6403ef908a98",
+    "url": "/drinkinggames/static/js/main.11f1de27.chunk.js"
+  },
+  {
+    "revision": "24aa7d9b15b7cc9ea867",
+    "url": "/drinkinggames/static/js/runtime-main.93448c3c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drinkinggames/static/media/logo.5d5d9eef.svg"
+  }
+]);
