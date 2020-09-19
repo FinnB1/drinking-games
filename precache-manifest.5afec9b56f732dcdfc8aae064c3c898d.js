d6f2114151ff6d86d0a0b60291fce51ad575f081
Finn <finnbutler1@gmail.com>
2020-09-19 17:05:36 +0100

    Updates

---
+++ precache-manifest.5afec9b56f732dcdfc8aae064c3c898d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b816153da37b9c749a9169b41306079",
+    "url": "/drinking-games/index.html"
+  },
+  {
+    "revision": "f2be87cc50a379f38f9b",
+    "url": "/drinking-games/static/css/2.7a1e5dfa.chunk.css"
+  },
+  {
+    "revision": "c03b4cd96fdf4da56bae",
+    "url": "/drinking-games/static/css/main.0186ecb3.chunk.css"
+  },
+  {
+    "revision": "f2be87cc50a379f38f9b",
+    "url": "/drinking-games/static/js/2.c7acd95a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/drinking-games/static/js/2.c7acd95a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c03b4cd96fdf4da56bae",
+    "url": "/drinking-games/static/js/main.b5249ea1.chunk.js"
+  },
+  {
+    "revision": "1cfca383fa5a81378a0d",
+    "url": "/drinking-games/static/js/runtime-main.9349834c.js"
+  },
+  {
+    "revision": "0a6b31c647e7ea7b5e148e795837b8fc",
+    "url": "/drinking-games/static/media/logo.0a6b31c6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/drinking-games/precache-manifest.82eef30b4534e128ffe55b920070570c.js"
+  "/drinking-games/precache-manifest.5afec9b56f732dcdfc8aae064c3c898d.js"
 );
 
 self.addEventListener('message', (event) => {
